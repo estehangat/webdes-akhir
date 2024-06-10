9246ac7e4a186050f38529a2a1503842c6a3c216
Daiva Paundra Gevano <96716034+estehangat@users.noreply.github.com>
2024-06-10 13:24:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function home(event){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     if(username == 'admin' && password == 'admin'){
-        window.location.href = '/library/index.html';
+        window.location.href = '/home/index.html';
     } else {
         document.getElementById('salah').innerHTML = '<br>Username atau Password salah!'
     }
