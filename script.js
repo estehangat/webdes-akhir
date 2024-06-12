4f88953fca23dcb6bbfc97bddbc0e5055bee785f
Daiva Paundra Gevano <96716034+estehangat@users.noreply.github.com>
2024-06-12 23:04:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function home(event){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     if(username == 'admin' && password == 'admin'){
+        sessionStorage.setItem('isLogin', 'true');
         window.location.href = '/home/index.html';
     } else {
         document.getElementById('salah').innerHTML = '<br>Username atau Password salah!'
